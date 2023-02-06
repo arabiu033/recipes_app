00a51741d5b65d189cffb8c2775e743c6cf0acf1
Okechukwu <okei.muokwugwo@gmail.com>
2023-02-06 19:58:22 +0100

    Generate foods controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'foods/index'
+  get 'foods/new'
+  get 'foods/create'
+  get 'foods/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "food#index"
+
+  resources :food, only: [:index, :new, :create, :destroy]
+  resources :recipes, only: [:index, :show, :create, :destroy]
+  resources :public_recipes, only: [:index, :show]
+  resources :general_shoping_list, only: [:index]
 end
