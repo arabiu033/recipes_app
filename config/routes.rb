086ce19d4b484a68f9b909670daea41c32c1d138
Aminu Rabiu <arabiu033@gmail.com>
2023-02-06 16:13:23 +0100

    Setup the project routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "food#index"
+
+  resources :food, only: [:index, :create]
+  resources :recipes, only: [:index, :show, :create, :destroy]
+  resources :public_recipes, only: [:index, :show]
+  resources :general_shoping_list, only: [:index]
 end
