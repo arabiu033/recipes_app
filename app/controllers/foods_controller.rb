873f07fe3299c6c5bc87112362ddabb34ed861cf
Okechukwu <okei.muokwugwo@gmail.com>
2023-02-06 20:18:07 +0100

    Configure foods controller actions

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,13 +1,30 @@
 class FoodsController < ApplicationController
   def index
+    @foods = Food.all
   end
 
   def new
+    @food = Food.new
   end
 
   def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to @food, notice: 'Food was successfully created.'
+    else
+      render :new
+    end
   end
 
   def destroy
+    @food =Food.find(params[:id])
+    @food.destroy
+    redirect_to @food, notice: 'Food was successfully destroyed.'
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :qunatity, :user_id)  
   end
 end
