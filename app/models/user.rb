e2bd529df65b4416898ae959133623edfc5a5194
Okechukwu <okei.muokwugwo@gmail.com>
2023-02-08 15:21:50 +0100

    setup devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_many :recipes, dependent: :delete_all
   has_many :foods, dependent: :delete_all
 
-  validates :name, presence: true, length: { in: 5..30 }
+  validates :name, presence: true
 end
