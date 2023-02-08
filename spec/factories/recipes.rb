069053b478127bea9e89ef4fe6fcf627f63bbb26
Aminu Rabiu <arabiu033@gmail.com>
2023-02-08 11:13:40 +0100

    Include the food in factorygirl

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -7,6 +7,6 @@ FactoryGirl.define do
     cooking_time { Faker::Number.decimal(l_digits: 1, r_digits: 1) }
     description { Faker::Hipster.paragraph_by_chars(characters: 356, supplemental: false) }
     public { Faker::Boolean }
-    user { recipe }
+    user { user }
   end
 end
