4477a7f5fdae394312ff620a1cadfc00590b4c72
Aminu Rabiu <arabiu033@gmail.com>
2023-02-08 09:17:05 +0100

    Re adjust the the controllers actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,20 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @recipes }
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    redirect_to recipes_path, notice: "Deleted post: #{recipe.name}"
+  end
 end
