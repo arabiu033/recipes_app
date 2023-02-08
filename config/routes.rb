fc7c419c052b3a51cef38c10375edcf599181bd5
Aminu Rabiu <arabiu033@gmail.com>
2023-02-08 11:14:30 +0100

    Configure the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  # get 'foods/index'
-  # get 'foods/new'
-  # get 'foods/create'
-  # get 'foods/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
