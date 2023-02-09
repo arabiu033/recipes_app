bc64bfa02c3413e675eb029f8a91f07185e94384
Aminu Rabiu <arabiu033@gmail.com>
2023-02-09 18:26:26 +0100

    Fix the shoping list query

--- app/controllers/general_shoping_lists_controller.rb
+++ app/controllers/general_shoping_lists_controller.rb
@@ -1,7 +1,8 @@
 class GeneralShopingListsController < ApplicationController
   def index
     @foods = Food.joins('LEFT JOIN recipe_foods ON
-      recipe_foods.food_id = foods.id').where('recipe_foods.food_id IS NULL')
+      recipe_foods.food_id = foods.id').where('recipe_foods.food_id IS NULL
+      AND foods.user_id = ?', current_user.id)
     @count = @foods.count
     @sum = @foods.sum('price * foods.quantity')
   end
