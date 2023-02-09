194e32415bb9f3a11d57a49bddc522efc5f991f1
Aminu Rabiu <arabiu033@gmail.com>
2023-02-09 13:45:47 +0100

    Fix linters

--- app/controllers/general_shoping_lists_controller.rb
+++ app/controllers/general_shoping_lists_controller.rb
@@ -1,6 +1,6 @@
 class GeneralShopingListsController < ApplicationController
   def index
-    @foods = Food.joins("LEFT JOIN recipe_foods ON recipe_foods.food_id = foods.id").where("recipe_foods.food_id IS NULL")
+    @foods = Food.joins('LEFT JOIN recipe_foods ON recipe_foods.food_id = foods.id').where('recipe_foods.food_id IS NULL')
     @count = @foods.count
     @sum = @foods.sum('price * foods.quantity')
   end
