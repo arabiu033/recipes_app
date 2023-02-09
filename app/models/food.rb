87a2f383a3345c2ed9daf3381daa01a9e51faa5d
Aminu Rabiu <arabiu033@gmail.com>
2023-02-09 08:18:35 +0100

    Provide some helper methods

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,4 +3,12 @@ class Food < ApplicationRecord
   has_many :recipe_foods, dependent: :delete_all
 
   validates :name, :measurement_unit, :price, :quantity, presence: true
+
+  def quantity_value
+    "#{quantity}#{measurement_unit}"
+  end
+
+  def value_price
+    "$#{price * quantity}"
+  end
 end
