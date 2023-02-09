2c845346c238101773224b6b7f943ec3ada5cb6b
Okechukwu <okei.muokwugwo@gmail.com>
2023-02-09 09:24:54 +0100

    Fix linter errors

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,6 @@
 class UserMailer < ApplicationMailer
-
   def confirmation_email(user)
     @user = user
-    mail(to: @user.email, subject: "Registration Confirmation")
+    mail(to: @user.email, subject: 'Registration Confirmation')
   end
-  
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :delete_all
-  
+
   validates :name, :measurement_unit, :price, :quantity, presence: true
 end
