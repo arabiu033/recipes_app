805f4e90b283a6c3204758e01dccbb0d046494cb
Aminu Rabiu <arabiu033@gmail.com>
2023-02-09 10:06:53 +0100

    Remove node-module

---
+++ app/controllers/general_shoping_lists_controller.rb
@@ -0,0 +1,5 @@
+class GeneralShopingListsController < ApplicationController
+
+  def index
+  end
+end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  load_and_authorize_resource
+  # load_and_authorize_resource
 
   def index
     @recipes = Recipe.all
