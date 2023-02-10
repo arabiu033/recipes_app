8131b9435e5311326fc62f509082e29d269bfea9
Aminu Rabiu <arabiu033@gmail.com>
2023-02-10 01:45:09 +0100

    Configure capybara

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,10 @@ FactoryGirl.define do
     name { Faker::Name.name }
     email { "#{Faker::Hipster.words(number: 1).join(' ')}@gmail.com" }
     password '123456'
+    trait :confirmed do
+      confirmed_at { Time.zone.now }
+      confirmation_sent_at { Time.zone.now }
+      confirmation_token { Faker::Name.name }
+    end
   end
 end
